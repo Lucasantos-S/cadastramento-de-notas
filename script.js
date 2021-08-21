26d34a394b3eb9c32179c8408c6a248af47d8757
lucasantos-S <lucassantos.dsilv@gmail.com>
2021-08-21 17:19:18 -0300

    finalizando o HTML

--- script.js
+++ script.js
@@ -1,38 +1,17 @@
-let nome = document.getElementById('fname')
-let sobre = document.getElementById('sname')
-let data = document.getElementById('fdata')
-let turma = document.getElementById('fturma')
-let nota = document.getElementById('fnota')
-let res = document.getElementById('res')
+class Aluno {
 
-       
-        
-
-
-
-function finalizar(){
-   
-    nome = String(nome.value)       
-    sobre = String(sobre.value)
-    data = String(data.value)
-    turma = String(turma.value)
-    nota= Number(nota.value)
-    if(let = nota >= 90 ){
-        nota = `A`
-    } else if(nota >= 80){
-        nota = `B`
+    constructor() {
 
     }
-   
-    res.innerHTML += ` O aluno ${nome}${sobre} que nasceu em ${data} da turma ${turma} tirou nota: ${nota}`
-    
-    
+
+    salvar(){
+        alert('ok')
+    }
+
+    limpar(){
+        alert('limpar')
+    }
 
 }
 
-    fnota.value = ''
-    fname.value = ''
-    sname.value = ''
-
-/*|| data.value.length == 0 || turma.value.length == 0 || nota.value.length == 0 
-res.innerHTML += `Aluno ${nome} ${sobre} `*/
+let aluno = new Aluno()
