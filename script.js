780f447fb6654b2f2712fde5818fdc529c186c1d
lucasantos-S <lucassantos.dsilv@gmail.com>
2021-08-23 18:58:11 -0300

    finalizando a validação dos campos

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ class Aluno {
         
         this.id = 1;
         this.arrayAlunos = [];
-        this.aluno
+        this.editId = null;
 
     }
 
@@ -12,6 +12,18 @@ class Aluno {
         
         let aluno = this.lerDados();
         console.log(aluno)
+
+        if(this.validarCampo(aluno)){
+            if(this.editId == null) {
+                this.adicionar(aluno);
+            } else{
+                this.adicionar(this.editId, aluno);
+            }
+        }
+        
+        
+    }
+    listaTabela(){
         
     }
 
@@ -39,9 +51,30 @@ class Aluno {
     validarCampo(aluno){
         let msg = ''
         if(aluno.nomeAluno ==''){
-            msg += '-Informe o nome do produto'
+            msg += '-Informe o nome do Aluno'
+        } 
+
+        if(aluno.turmaAluno == '') {
+            msg = '- Informe a turma'
         }
 
+        if(aluno.notaAluno == '') {
+            msg = '- Informe a nota do Aluno'
+        }
+        if(aluno.dataAluno == '') {
+            msg = '- Informe a data de lançamento'
+        }
+
+        if(aluno.professorAluno == '') {
+            msg = '- Informe o nome do Professor responsavel.'
+        }
+
+        if(msg!= '') {
+            alert(msg);
+            return false
+        }
+            return true
+        
     }
 
     
