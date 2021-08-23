51648d2854e35adaaffe83f79892f630c4c82e14
lucasantos-S <lucassantos.dsilv@gmail.com>
2021-08-23 18:35:16 -0300

    finalizando a leitura dos Array

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
 class Aluno {
 
     constructor() {
+        
+        this.id = 1;
+        this.arrayAlunos = [];
+        this.aluno
 
     }
 
     salvar(){
-        alert('ok')
+        
+        let aluno = this.lerDados();
+        console.log(aluno)
+        
     }
+
+    adicionar(aluno) {
+        this.arrayAlunos.push(aluno);
+        this.id++
+
+    }
+
+    lerDados(){
+        let aluno = {}
+        aluno.id = this.id;
+        aluno.nomeAluno = document.getElementById('aluno').value;
+        aluno.turmaAluno = document.getElementById('turma').value;
+        aluno.notaAluno = document.getElementById('nota').value;
+        aluno.dataAluno = document.getElementById('data').value;
+        aluno.professorAluno = document.getElementById('professor').value;
+
+
+        return aluno;
+
+       
+    }
+
+    validarCampo(aluno){
+        let msg = ''
+        if(aluno.nomeAluno ==''){
+            msg += '-Informe o nome do produto'
+        }
+
+    }
+
+    
+    
 
     limpar(){
         alert('limpar')
